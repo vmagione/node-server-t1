9254edbaa9bcf74461016401564542e1f50bd5d5
Vinnicius Magione <vinniciussoares@hotmail.com>
2026-03-02 19:47:07 -0300

    Create in memory database
    Create in memory database

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Criando server sem fastify
+
+/*
 const { createServer } = require('node:http');
 
 const hostname = '127.0.0.1';
@@ -18,3 +21,38 @@ const server = createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+*/
+
+
+// Criando server com fastify
+
+import { fastify } from "fastify";
+import { DatabaseMemory } from "./database-memory.js";
+
+const server = fastify();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+server.post("/videos", () => {
+    return "Server created!";
+});
+
+server.get("/videos", () => {
+    return "Route hello created!";
+});
+
+server.put("/videos:id", () => {
+    return "Server created!";
+});
+
+server.delete("/videos:id", () => {
+    return "Server created!";
+});
+
+server.listen({
+    hostname: hostname,
+    port: port,
+});
+
