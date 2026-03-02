8cc80d1edc653b01c6fc846e7ee0197bfa6203a0
Vinnicius Magione <vinniciussoares@hotmail.com>
2026-03-02 19:15:18 -0300

    Create package.json and dev script
    Create package.json and dev script

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const { createServer } = require('node:http');
+
 const hostname = '127.0.0.1';
 const port = 3000;
+
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Server created!');
+    console.log("Log: Server created!");
+
+   
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+
+    res.write("Response: writed");
+    return res.end('Server created!');
+  
 });
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
